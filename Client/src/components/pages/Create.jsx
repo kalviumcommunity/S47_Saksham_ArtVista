de96325e91247972fde275f093365cf2d0aad679
sparky-sg <sg.saksham2005@gmail.com>
2024-03-13 08:38:51 +0530

    more improvements

--- Client/src/components/pages/Create.jsx
+++ Client/src/components/pages/Create.jsx
@@ -10,6 +10,37 @@ function Create() {
       <div>
         <br /><br /><br /><br /><br />
         <h1>Create area</h1>
+          
+        <form action="">
+            <div>
+              <label  htmlFor="title">Title: </label>
+              <input
+              type='text' 
+              placeholder='test'
+              onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            
+            <div>
+              <label htmlFor="image">File upload</label>
+              <input
+              type='file'
+              name='file' 
+              placeholder='******'
+              onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label  htmlFor="description">Description: </label>
+              <input
+              type='text' 
+              placeholder='test'
+              onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+        </form>
+        
       </div>
       <Footer />
     </>
--- Client/src/components/pages/Search.jsx
+++ Client/src/components/pages/Search.jsx
@@ -8,9 +8,34 @@ function Search() {
     <>
       <Navbar />
       <br /><br /><br /><br /><br />
-      <div>
-        <h1>Search</h1>
-        <input type="text" />
+      <div className={`${Searchcss.fullflex}`}>
+        <div  className={`${Searchcss.container}`}>
+          
+        <div className={`${Searchcss.searchflex}`}>
+          <div className={`${Searchcss.searchflextt}`}>
+            <p>Search By Title</p>
+            <input type="text" />
+          </div>
+          <div>
+            <p>Search By Author</p>
+            <input type="text" />
+          </div>
+        {/* </div>
+
+
+        <div className={`${Searchcss.searchflex}`}> */}
+          <div>
+            <p>Search By Date</p>
+            <input type="date" />
+          </div>
+          <div  className={`${Searchcss.searchflextt}`}>
+            <p>Search By Categories</p>
+            <input type="text" />
+          </div>
+        </div>
+        
+        <button>Start Searching</button>
+        </div>        
       </div>
       <Footer />
     </>
