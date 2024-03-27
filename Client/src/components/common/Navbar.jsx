81647d38cd5f38f93e8678c4601b7f621644318e
sparky-sg <sg.saksham2005@gmail.com>
2024-03-27 14:43:22 +0530

    redirection added (not functioning as required)

--- Client/src/components/common/Navbar.jsx
+++ Client/src/components/common/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import navcss from './css/Navbar.module.css'
+import Redirect from './Redirect'
 function Navbar() {
   return (
     <>
@@ -10,10 +11,10 @@ function Navbar() {
             <Link className={navcss.links} to="/"><p>Home</p></Link>
             <Link className={navcss.links} to="/search"><p>Search</p></Link>
             <Link className={navcss.links} to="/create"><p>Create</p></Link>
-            <Link className={navcss.links} to="/myprofile"><p>My Profile</p></Link>
+            {/* <Link className={navcss.links} to="/myprofile"><p>My Profile</p></Link> */}
         </div>
         <div className={navcss.loginsignupdiv}>
-            <Link className={navcss.loginsignup} to="/auth/login">Login/Signup</Link>
+            <Redirect />
         </div>
     </div>
     </>
