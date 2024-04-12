a47b648ed47c90f85b74905c15239b8124e70a7d
sparky-sg <sg.saksham2005@gmail.com>
2024-04-12 12:18:56 +0530

    made application more solid to login logout requests to profile picture setups

--- Client/src/components/pages/UserPosts.jsx
+++ Client/src/components/pages/UserPosts.jsx
@@ -3,12 +3,15 @@ import axios from 'axios'
 import homecss from './css/Home.module.css'
 import { useNavigate } from 'react-router-dom';
 
+//loader 
+import Loader from '../common/components/loader'
 const OtherEdit = () => {
 
     const [posts, setPosts] = useState([]);
     const [userId, setUserId] = useState('');
     const [editPostId, setEditPostId] = useState(null);
     const navigate = useNavigate();
+    const [loader, setLoader] = useState(true);
 
     useEffect(() => {
         const loggedInUser = localStorage.getItem('loggedInUser');
@@ -24,9 +27,11 @@ const OtherEdit = () => {
           try {
             const response = await axios.get(import.meta.env.VITE_HOMEAPI);
             setPosts(response.data);
+            setLoader(false);
             // console.log(response.data)
           } catch (error) {
             console.error('Error fetching data:', error);
+            setLoader(false);
           }
         };
     
@@ -42,37 +47,42 @@ const OtherEdit = () => {
     <>
         <br /><br /><br /><br /><br /><br />
         <div className={homecss.container}>
-        <div className={homecss.postscont}>
         {
-            posts
-                .filter((post) => post.email == userId)
-                .map((post,index)=>{
-                    return(
-                        <div className={homecss.postinv} key={post._id}>
-                        <div className={homecss.postimagecont}>
-                        <img 
-                        src={post.image} 
-                        alt="post" 
-                        className={homecss.postimage}
-                        />
-                        </div>
+            loader ? (
+              <Loader/>
+            ) : (
+              <div className={homecss.postscont}>
+            {
+                posts
+                    .filter((post) => post.email == userId)
+                    .map((post,index)=>{
+                        return(
+                            <div className={homecss.postinv} key={post._id}>
+                            <div className={homecss.postimagecont}>
+                            <img 
+                            src={post.image} 
+                            alt="post" 
+                            className={homecss.postimage}
+                            />
+                            </div>
 
-                        <div className={homecss.postdetails}>
-                        <h3>{post.title}</h3>
-                        <p>{post.description}</p>
-                        <div>
+                            <div className={homecss.postdetails}>
+                            <h3>{post.title}</h3>
+                            <p>{post.description}</p>
                             <div>
-                            <button onClick={() => handleEdit(post._id)}>Edit</button>
-                            <button>Delete</button>
+                                <div>
+                                <button onClick={() => handleEdit(post._id)}>Edit</button>
+                                <button>Delete</button>
+                                </div>
+                            </div>
                             </div>
                         </div>
-                        </div>
-                    </div>
-                    )
-                })
-        }
-        
+                        )
+                    })
+            }
         </div>
+            )
+        }
         </div>
     </>
   )
